ef2ce8a591b418449a67119b3fd1b36cc955b749
MarkEcza <dek0039@edu.curio.nl>
2026-03-24 15:46:45 +0100

    Product Images

---
+++ Helpers/Converter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media.Imaging;
+using System;
+using System.IO;
+
+namespace shittyEtsy.Converters
+{
+    public class ByteArrayToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is not byte[] bytes || bytes.Length == 0)
+                return null;
+
+            var bitmap = new BitmapImage();
+            using var ms = new MemoryStream(bytes);
+            var stream = ms.AsRandomAccessStream();
+            bitmap.SetSourceAsync(stream).GetResults();
+            return bitmap;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+            => throw new NotImplementedException();
+    }
+}
--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -1,28 +1,16 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Imaging;
 using Microsoft.UI.Xaml.Navigation;
-using shittyEtsy.Data.Models;
+using shittyEtsy.Views;
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-
-// To learn more about WinUI, the WinUI project structure,
-// and more about our project templates, see: http://aka.ms/winui-project-info.
+using System.Threading.Tasks;
 
 namespace shittyEtsy.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class HomePage : Page
     {
         public HomePage()
@@ -30,13 +18,32 @@ namespace shittyEtsy.Views
             InitializeComponent();
         }
 
-        protected override void OnNavigatedTo(NavigationEventArgs e)
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
         {
-            using var Db = new Data.AppDataContext();
-            var products = Db.Product.ToList();
-            productsList.ItemsSource = products;
+            using var db = new Data.AppDataContext();
+            var products = db.Product.ToList();
+
+            var viewModels = new List<ProductViewModel>();
+
+            foreach (var p in products)
+            {
+                var vm = new ProductViewModel { Product = p };
+
+                if (p.ImageData != null && p.ImageData.Length > 0)
+                {
+                    var bitmap = new BitmapImage();
+                    using var ms = new MemoryStream(p.ImageData);
+                    var stream = ms.AsRandomAccessStream();
+                    await bitmap.SetSourceAsync(stream);
+                    vm.Image = bitmap;
+                }
+
+                viewModels.Add(vm);
+            }
+
+            productsList.ItemsSource = viewModels;
         }
-        
+
         private void GoToCreateProductPage_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(CreateProductPage));
